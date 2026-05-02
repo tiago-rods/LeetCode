a03b4a7e519047c502ecb57aa5d2f5c799247a19
tiago-rods <t.alves.rodrigues@hotmail.com>
2026-05-02 15:00:14 -0300

    solved 21. 0ms, 19.2MB

---
+++ Medium/12.integerToRoman.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        roman_num = ''
+        for i in range(len(val)):
+            while num >= val[i]:
+                roman_num += syb[i]
+                num -= val[i]
+        return roman_num    
+    
