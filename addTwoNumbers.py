fc4de5e29ca8c746fa49d48940f30a9e238188bd
tiago-rods <t.alves.rodrigues@hotmail.com>
2026-05-02 14:48:59 -0300

    solved: 2.addTwoNumbers 3ms, 19.5MB

---
+++ addTwoNumbers.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(0)
+        current = head
+        carry = 0
+        while l1 or l2 or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            nextVal = l1Val + l2Val + carry
+            current.next = ListNode(nextVal % 10)
+            carry = nextVal // 10
+            current = current.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return head.next    
