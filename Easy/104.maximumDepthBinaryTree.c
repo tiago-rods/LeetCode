4bfe6c293e9c05c44d6c2c7d0a45feadf9b0383c
tiago-rods <t.alves.rodrigues@hotmail.com>
2026-05-06 23:17:50 -0300

    solved 104. em C, 0ms O(n), 12.1MB O(w) sendo w profundidade da arvore

---
+++ Easy/104.maximumDepthBinaryTree.c
@@ -0,0 +1,16 @@
+
+// Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+ };
+
+int maxDepth(struct TreeNode* root) {
+    if(!root) return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return 1 + (left > right ? left : right);
+}
